4a76c5da7560275e672e6be74326a8c1bca1c8bc
wangym <wangym0203@gmail.com>
2023-10-09 20:46:30 +0800

    9732: fuej pow fewo

--- pr-create.sh
+++ pr-create.sh
@@ -12,29 +12,14 @@ branch_name=${jira_ticket}--$(echo "$issue_name" | sed 's/ /-/g')
 commit_title=${jira_ticket}': '${issue_name}
 
 create_new_branch=$(git checkout -b "${branch_name}")
-create_new_commit=$(git add . && git commit -m "${commit_title}")
+create_new_commit=$(git add . && git commit -m "${commit_title}" && git push)
 
 echo "${create_new_branch}"
 echo "${create_new_commit}"
 
-pr_body="
-# PR Ready
-
-## Types of changes
-- [ ] Bug fix (non-breaking change which fixes an issue)
-- [ ] New feature (non-breaking change which adds functionality)
-- [ ] Refactoring (non-breaking change which does not change functionality)
-
-#### Short description:
-
-
-#### Trello Issue Link:
-
-
-#### Dependency
-"
+pr_body='fweoiewf12121212'
 
 # must provide `--title` and `--body` (or `--fill` or `fill-first`) when not running interactively
-pr_url=$(gh pr create --title "${commit_title}" --body "${pr_body}" -H)
+pr_url=$(gh pr create --title "${commit_title}" --body "${pr_body}" -H $branch_name)
 
 echo "${pr_url}"
