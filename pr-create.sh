cb7f8cb0a99a34528eebc1c555316e9c0fa15ec3
wangym <wangym0203@gmail.com>
2023-10-10 22:59:17 +0800

    add pr-merge.sh file and update pr-create.sh

--- pr-create.sh
+++ pr-create.sh
@@ -6,13 +6,14 @@ read -p 'issue name: ' issue_name
 
 # read -p 'Types of changes: ' types_of_changes
 
-# read -p 'Short description: ' short_description
+read -p 'Short description: ' short_description
 
 branch_name=${jira_ticket}--$(echo "$issue_name" | sed 's/ /-/g')
 commit_title=${jira_ticket}': '${issue_name}
 
 create_new_branch=$(git checkout -b "${branch_name}")
-create_new_commit=$(git add . && git commit -m "${commit_title}")
+sleep 1
+create_new_commit=$(git add . && git commit -m "${commit_title}" && git push)
 
 echo "${create_new_branch}"
 echo "${create_new_commit}"
@@ -27,6 +28,7 @@ pr_body="
 
 #### Short description:
 
+$short_description
 
 #### Trello Issue Link:
 
@@ -37,4 +39,20 @@ pr_body="
 # must provide `--title` and `--body` (or `--fill` or `fill-first`) when not running interactively
 pr_url=$(gh pr create --title "${commit_title}" --body "${pr_body}" -H)
 
-echo "${pr_url}"
+sleep 1
+
+echo $pr_url
+
+echo | $(jira issue comment add "${jira_ticket}" "${pr_url}")
+
+# write history
+script_dir="$(dirname "$0")"
+
+pr_id=$(echo "$pr_url" | grep -oE '[0-9]+$')
+
+echo "${jira_ticket},${pr_id}" >> "${script_dir}/work-history.txt"
+
+# TODO change jira status
+# TODO add Types of changes
+# TODO open pr on windows
+# TODO add jira assign to me
