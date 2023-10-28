1dbce2d38b88bac3917018711f227e0593ff2b45
wangym <wangym0203@gmail.com>
2023-10-28 20:24:56 +0800

    Optimize work history search matching

---
+++ history.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+script_dir="$(dirname "$0")"
+
+write_history() {
+    local pr_url=$1
+    local jira_ticket=$2
+
+    pr_id=$(echo "$pr_url" | grep -oE '[0-9]+$')
+    new_history="${jira_ticket},${pr_id}"
+
+    echo $new_history >>"${script_dir}/work-history.txt"
+
+    echo ✓ Added a work history in work-history.txt: $new_history
+}
+
+read_history() {
+    local pr_id=$1
+
+    text=$(grep -E ",$pr_id$" "${script_dir}/work-history.txt")
+
+    if [ -z "$text" ]; then
+        return 0
+    fi
+
+    jira_ticket=$(echo "$text" | awk -F ',' '{print $1}')
+
+    echo "$jira_ticket"
+}
--- todolist.md
+++ todolist.md
@@ -4,6 +4,7 @@
      - [x] [P0] The network checks that it is not possible to determine whether the git push is available in the command
      - [x] [P1] Exceptions may occur when there is no incoming issue with the `issue desc` option
      - [x] [P1] The default `types of changes` option cannot work
+     - [x] [p1] Optimize work history search matching
 3. - [ ] Feature
      - [ ] Be able to install our program
      - [ ] Add green right flag
