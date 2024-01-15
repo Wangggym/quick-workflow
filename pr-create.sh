05899e8b6bf4ee410f6879e6fba71ad8c97be4f5
wangym <wangym0203@gmail.com>
2024-01-15 17:50:36 +0800

    optimize pr create and pr merge flow (#20)

--- pr-create.sh
+++ pr-create.sh
@@ -24,8 +24,25 @@ if [ -n "${jira_ticket}" ]; then
 fi
 
 if [ -n "${jira_ticket}" ]; then
-    issue_desc=$(aiwflow pr-create $jira_ticket)
-    echo -e $y 'PR title: '$issue_desc
+    issue_json=$(aiwflow issue-desc $jira_ticket)
+    issue_desc=$(echo "$issue_json" | jq -r '.issue_desc')
+    need_translate=$(echo "$issue_json" | jq -r '.need_translate')
+    translated_desc=$(echo "$issue_json" | jq -r '.translated_desc')
+
+    if [ "$issue_desc" != "null" ]; then
+        echo -e $y 'PR title: '$issue_desc
+        if [ "$need_translate" = "true" ]; then
+            if [ "$translated_desc" != "null" ]; then
+                issue_desc=$translated_desc
+                echo -e $y 'Chat GPT translated: '$translated_desc
+            else
+
+                echo -e $n 'Chat GPT translated failed, but it dose not affect the use.'
+            fi
+        fi
+    else
+        issue_desc=""
+    fi
 fi
 
 while [ -z "$issue_desc" ]; do
