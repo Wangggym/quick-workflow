66a418389fae948d26395bdaf9377afd82239754
wangym <wangym0203@gmail.com>
2024-10-09 09:32:34 +0800

    pr titile as git commit titile when new commit pushed.

--- get-pr-titile.sh
+++ get-pr-titile.sh
@@ -7,7 +7,10 @@ source "$script_dir/base.sh"
 pr_title=$(gh pr view --json title --jq .title)
 
 if [ -n "$pr_title" ]; then
-    echo -e "${y}PR Title: $pr_title"
+    echo -e "${y} get pr title: $pr_title"
+    git add --all && git commit -m "$pr_title"
 else
-    echo -e "${r}Failed to retrieve PR title."
+    echo -e "${r} Failed to retrieve PR title."
+    git add --all && git commit -m "update"
 fi
+
